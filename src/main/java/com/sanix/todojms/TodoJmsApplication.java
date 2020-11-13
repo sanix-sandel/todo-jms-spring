6060584cf1f15a625a3d080fb1b71f2b40cc15b7
sanix <sanicksikani@gmail.com>
2020-11-13 20:04:22 +0300

    Producer

---
+++ src/main/java/com/sanix/todojms/TodoJmsApplication.java
@@ -0,0 +1,13 @@
+package com.sanix.todojms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanix/todojms/TodoJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanix.todojms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
